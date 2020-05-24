70d857041c267a9c79e67a590ecc307e5f1bfe9a
JamesQFreeman <ws.bond1@gmail.com>
2020-05-24 23:04:46 +0800

    dataset initializer's display

--- utils/dataset_initializer.py
+++ utils/dataset_initializer.py
@@ -50,11 +50,17 @@ class Data():
     def setFoldNum(self, fold_num):
         self.fold_num = fold_num
 
+
 # Dataset class should be an dict {id:Data,...}
-
-
 class Dataset():
     def __init__(self, data_dir, res_dir):
+
+        # This initializer can be divided into several parts as shown below
+        # 1. Get all files' direction
+        # 2. Process them to a list item for further use
+        # 3. initialize the internal self.dataset
+
+        # ------------PART 1----------------
         # first * for the fold num
         # second * for the id
         img_list = glob.glob(data_dir+'/*/*/thin.nii.gz')
@@ -64,46 +70,74 @@ class Dataset():
         seg_f_list = glob.glob(res_dir+'/*/prob_2.mhd')
         seg_b_list = glob.glob(res_dir+'/*/seg.nii.gz')
 
+        # -------------PART 2-------------
         # id = img_dir.split('/')[-2]
         # fold_num = img_dir.split('/')[-3]
         # img_dir = img_dir
         # same thing for the label
-        img_list = [[img_dir.split('/')[-2], img_dir.split('/')[-3], img_dir] 
+        img_list = [[img_dir.split('/')[-2], img_dir.split('/')[-3], img_dir]
                     for img_dir in img_list]
-        label_list = [[label_dir.split('/')[-2], label_dir.split('/')[-3], label_dir] 
-                    for label_dir in label_list]
+        label_list = [[label_dir.split('/')[-2], label_dir.split('/')[-3], label_dir]
+                      for label_dir in label_list]
 
-        
-        seg_f_list = [[seg_f_dir.split('/')[-2], seg_f_dir] 
-                    for seg_f_dir in seg_f_list]
-        seg_b_list = [[seg_b_dir.split('/')[-2], seg_b_dir] 
-                    for seg_b_dir in seg_b_list]
+        seg_f_list = [[seg_f_dir.split('/')[-2], seg_f_dir]
+                      for seg_f_dir in seg_f_list]
+        seg_b_list = [[seg_b_dir.split('/')[-2], seg_b_dir]
+                      for seg_b_dir in seg_b_list]
 
         # this is for debug use
-        # maybe we should debug this first
-        print('img_list_content:',img_list[:3])
-        print('label_list_content:',label_list[:3])
-        print('seg_f_list_content:',seg_f_list[:3])
-        print('seg_b_list_content:',seg_b_list[:3])
+        # print('img_list_content:', img_list[:3])
+        # print('label_list_content:', label_list[:3])
+        # print('seg_f_list_content:', seg_f_list[:3])
+        # print('seg_b_list_content:', seg_b_list[:3])
 
-        pass
+        # ------------PART 3------------
+
+        # Now we should get all the id and initialize Data object for them
+        self.dataset = {}
+        id_list = [x[0] for x in img_list]
+        for _id in id_list:
+            dataset[_id] = Data()
+
+        # Now we set them
+        for item in img_list:
+            _id, fold_num, img_dir = item
+            self.dataset[_id].setImage(img_dir)
+            self.dataset[_id].setFoldNum(fold_num)
+
+        for item in label_list:
+            _id, _, label_dir = item
+            self.dataset[_id].setLabel(label_dir)
+
+        for item in seg_f_list:
+            _id, seg_f_dir = item
+            self.dataset[_id].setCoarseSegFloat(seg_f_dir)
+
+        for item in seg_b_list:
+            _id, seg_b_dir = item
+            self.dataset[_id].setCoarseSegBool(seg_b_dir)
+
+        print('A VoxelRend Dataset have been initialized from {} and {}'\
+            .format(data_dir, res_dir))
+        print('Consist of {} cases in total.'.format(len(img_list)))
+        print('{} of them have coarse segmentation'.format(len(seg_b_list)))
 
     def _splitLastThreeDir(self, d: str) -> List[str]:
         # get some dir and return the last three dir/file
         l = d.split('/')
         return [l[-3], l[-2], l[-1]]
 
-    def getImage(self, id):
+    def getImage(self, _id):
         return img
 
-    def getLabel(self, id):
+    def getLabel(self, _id):
         pass
 
-    def getSegPossibility(self, id):
+    def getSegPossibility(self, _id):
         pass
 
-    def getSeg(slef, id):
+    def getSeg(slef, _id):
         pass
 
 
-Dataset(ORIGINAL_DATA_DIR,COARSE_DIRECTION_RESULT_DIR)
+Dataset(ORIGINAL_DATA_DIR, COARSE_DIRECTION_RESULT_DIR)
